9a90a0d89ba5ccf6214381ed27babd4f54460ad0
shucc <cc@cchao.org>
2017-03-22 14:33:35 +0800

    左侧侧边栏滑出时状态栏及虚拟按键显示处理

--- app/src/main/java/com/android/launcher3/side/left/LeftFragment.java
+++ app/src/main/java/com/android/launcher3/side/left/LeftFragment.java
@@ -1,13 +1,16 @@
 package com.android.launcher3.side.left;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.android.launcher3.R;
+import com.android.launcher3.util.ScreenUtil;
 
 /**
  * Created by shucc on 17/3/22.
@@ -15,10 +18,54 @@ import com.android.launcher3.R;
  */
 public class LeftFragment extends Fragment {
 
+    private final String TAG = "LeftFragment";
+
+    private View viewStatusBar;
+    private View viewNavigationBar;
+
+    private Activity activity;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        activity = getActivity();
         View rootView = inflater.inflate(R.layout.fragment_letf_side, container, false);
+        initViews(rootView);
         return rootView;
+    }
+
+    private void initViews(View rootView) {
+        viewStatusBar = rootView.findViewById(R.id.view_statusBar);
+        viewNavigationBar = rootView.findViewById(R.id.view_navigationBar);
+
+        initStatusNavigation();
+    }
+
+    /**
+     * 处理状态栏和底部虚拟按键
+     */
+    private void initStatusNavigation() {
+        int statusHeight = ScreenUtil.getStatusHeight(activity);
+        int navigationHeight = ScreenUtil.getBottomStatusHeight(activity);
+
+        if (statusHeight == 0) {
+            viewStatusBar.setVisibility(View.GONE);
+        } else {
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) viewStatusBar.getLayoutParams();
+            params.height = statusHeight;
+            params.width = RelativeLayout.LayoutParams.MATCH_PARENT;
+            viewStatusBar.setLayoutParams(params);
+            viewStatusBar.setVisibility(View.VISIBLE);
+        }
+
+        if (navigationHeight == 0) {
+            viewNavigationBar.setVisibility(View.GONE);
+        } else {
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) viewNavigationBar.getLayoutParams();
+            params.height = navigationHeight;
+            params.width = RelativeLayout.LayoutParams.MATCH_PARENT;
+            viewNavigationBar.setLayoutParams(params);
+            viewNavigationBar.setVisibility(View.VISIBLE);
+        }
     }
 }
---
+++ app/src/main/java/com/android/launcher3/util/ScreenUtil.java
@@ -0,0 +1,83 @@
+package com.android.launcher3.util;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by shucc on 17/3/22.
+ * cc@cchao.org
+ */
+public class ScreenUtil {
+
+    private ScreenUtil() {
+    }
+
+    private static int getDpi(Context context) {
+        int dpi = 0;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        @SuppressWarnings("rawtypes")
+        Class c;
+        try {
+            c = Class.forName("android.view.Display");
+            @SuppressWarnings("unchecked")
+            Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+            method.invoke(display, displayMetrics);
+            dpi = displayMetrics.heightPixels;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dpi;
+    }
+
+    /**
+     * 获得屏幕高度
+     *
+     * @param context
+     * @return
+     */
+    private static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.heightPixels;
+    }
+
+    /**
+     * 获取 虚拟按键的高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getBottomStatusHeight(Context context) {
+        int totalHeight = getDpi(context);
+        int contentHeight = getScreenHeight(context);
+        return totalHeight - contentHeight;
+    }
+
+    /**
+     * 获取状态栏高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getStatusHeight(Context context) {
+        int statusHeight = -1;
+        try {
+            Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+            Object object = clazz.newInstance();
+            int height = Integer.parseInt(clazz.getField("status_bar_height")
+                    .get(object).toString());
+            statusHeight = context.getResources().getDimensionPixelSize(height);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return statusHeight;
+    }
+}
